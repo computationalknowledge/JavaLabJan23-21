e9fc93f527050ed9199a8d3b76696ba0846e58c6
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-01-23 13:16:40 -0500

    Create SimplestProgram.java

---
+++ SimplestProgram.java
@@ -0,0 +1,33 @@
+// comments
+// The simplest Java Project:
+// exactly 1 public class:
+// the public class must have a main method
+// class has:
+	// methods (behaviors - how things happen)
+	// data
+	// code must be contained in methods
+
+
+public class Dog{
+	public static void main(String[] args){
+		System.out.println("Hello  !!!"); 
+	}
+
+// implementation: specify the details of your class in {}
+
+	// what can I put into a class ??
+	// there are ONLY 2 kinds of things I can put into a Class:
+	// data  : is information about things
+	// methods : do things 
+
+	// lets give our Dog some data:
+	public String name; 
+	public int age;
+
+	// let's made a method for our dog:
+	
+	public void SayHello(){
+
+
+	}
+}
